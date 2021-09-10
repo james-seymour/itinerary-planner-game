e7be1f1f999c37c82626a2c7c5876463fcdc7c6d
james-seymour <jsjamesemail@gmail.com>
2021-09-11 00:04:36 +1000

    car example

--- src/components/game/Car.jsx
+++ src/components/game/Car.jsx
@@ -1,22 +1,28 @@
+import { makeStyles } from '@material-ui/styles'
 import React from 'react'
 
-function Car(props) {
 
-  const styles = {
-    rectangle: {
-      width: '50px',
-      height: '50px',
-      color: 'red'
-    }
+
+const useStyles = makeStyles((theme) => ({
+  // Can use the theme passed in here as well
+  rectangle: {
+    //CSS properties here
+    width: 100,
+    height: 100,
   }
+}))
 
+
+const Car = (props) => {
+  
+  const classes = useStyles()
+  
   return (
     <div>
-        <div style={styles.rectangle}></div>
+        <div style={classes.rectangle}></div>
     </div>
   )
 }
-
 
 
 export default Car
