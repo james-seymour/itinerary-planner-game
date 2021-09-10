b20b16542acdfe0b8a80d4c57ffa62a8e24bd846
james-seymour <jsjamesemail@gmail.com>
2021-09-11 02:56:28 +1000

    getWeatherData completed!

--- src/api/getWeatherData.js
+++ src/api/getWeatherData.js
@@ -6,30 +6,27 @@ import { getNearbyStationOptions,
 
 const getWeatherData = (values, APIData, setRedirect) => {
   const { latitude, longitude } = APIData
-  // Check this unpacks properly
   const { forecastStartDate, forecastEndDate } = values
+ 
   const nearbyStationOptions = getNearbyStationOptions(latitude, longitude)
-  // Check this returns a proper station ID
   axios.request(nearbyStationOptions).then((stationsResponse) => {
     return stationsResponse.data.data[0].id
   }).then((stationId) => {
+
     const weatherConditionOptions = getWeatherConditionOptions(stationId, forecastStartDate)
     const weatherHistoryOptions = getWeatherHistoryOptions(stationId, forecastStartDate, forecastEndDate)
-    // Something wrong here (seems to catch an error and redirect before getting here) 
     axios.request(weatherConditionOptions).then((weatherConditionResponse) => {
     axios.request(weatherHistoryOptions).then((weatherHistoryResponse) => {
+
       Object.assign(APIData, {
         weatherCondition: weatherConditionResponse.data.data,
         weatherHistory: weatherHistoryResponse.data.data,
       })
+
       setRedirect(true)
     }).catch((error) => { setRedirect(true) })
     }).catch((error) => { setRedirect(true) })
-  }).catch((error) => {
-    console.error(error)
-    setRedirect(true)
-  })
-
+  }).catch((error) => { setRedirect(true) })
 }
 
 export default getWeatherData
