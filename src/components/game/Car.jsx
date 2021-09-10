71b14d60ad215fc16c7c2ca6a00eeda4143adc73
sllngshot <slingshotutube@gmail.com>
2021-09-11 00:50:24 +1000

    test car in process

--- src/components/game/Car.jsx
+++ src/components/game/Car.jsx
@@ -1,9 +1,20 @@
+import { makeStyles } from '@material-ui/core'
 import React from 'react'
 
-export default function Car() {
-  return (
-    <div>
-      
-    </div>
-  )
+const useStyles = makeStyles((theme) => ({
+  car: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    backgroundColor: theme.palette.primary.main
+  }
+}))
+
+function Car() {
+
+  const classes = useStyles()
+
+  return <div className={classes.car}>deez nuts</div>
 }
+
+export default Car
--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
+import Car from './Car'
 
 class Game extends Component {
   
   constructor(props) {
-
+    super(props);
+    
     this.state = {
       playerX: 100,
       playerY: 100,
-      playerWidth: 10,
-      playerHeight: 10,
       playerDir: 'n',
       playerCanMove: true
     };
-    
+
   }
 
   render() {
     return <div onKeyDown={this.onKeyDown} tabIndex="0">
-
+        <Car />
       </div>
   }
 
