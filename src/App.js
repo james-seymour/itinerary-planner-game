a459dea108339cbcada5ec856feba4ae9d5165e5
james-seymour <jsjamesemail@gmail.com>
2021-09-10 22:27:24 +1000

    Setup React Router and added theme provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import { Router, Switch, Route } from "react-router-dom"
 import { ThemeProvider } from "@material-ui/core"
 import { createTheme } from '@material-ui/core/styles';
 import { orange, blue } from '@material-ui/core/colors';
+import { Game } from "./components/game/Game"
+import { InputForm } from "./components/input/InputForm"
 
 
 const theme = createTheme({
@@ -14,15 +17,30 @@ const theme = createTheme({
     }
   },
   typography: {
-    fontSize
+    fontSize: 12,
+    fontFamily: [
+      "Roboto",
+    ]
   }
 })
 
 function App() {
   return (
-    <div className="App">
-        "caca pooppoo"
-    </div>
+    <ThemeProvider theme={theme}>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <InputForm />
+          </Route>
+          <Route exact path="/game">
+            <Game />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+
   );
 }
 
