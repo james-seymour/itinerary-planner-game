9084a33a80fbd0ae37c411e398dbb5b30db16ef7
james-seymour <jsjamesemail@gmail.com>
2021-09-10 23:01:35 +1000

    Created first field for Formik MUI input form

--- src/components/input/InputForm.jsx
+++ src/components/input/InputForm.jsx
@@ -1,12 +1,62 @@
+import { Collapse, Typography } from "@material-ui/core"
+import "@fontsource/roboto"
+import { Field, Formik } from "formik"
 
+import { MuiPickersUtilsProvider } from "@material-ui/pickers"
+import DateFnsUtils from "@date-io/date-fns/"
+import { KeyboardDatePicker } from "formik-material-ui-pickers"
 
 
 const InputForm = (props) => {
 
   return (
-    <div>
-      This is an example div
-    </div>
+
+    <Formik
+      initialValues= {{
+        destination: "",
+        startDate: new Date(),
+        endDate: new Date(),
+        priceLevel: "medium",
+        numberOfPeople: "1"
+      }}
+    
+      validate={(values) => {
+        const errors = {}
+        return errors
+      }}
+
+      onSubmit={(values, {setSubmitting}) => {
+        setSubmitting = false
+        // GET API DATA HERE
+      }}
+    >
+      {({submitForm, isSubmitting, touched, errors}) => (
+        <>
+          <Collapse>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Field
+                component={KeyboardDatePicker}
+                name="startDate"
+                type="startDate"
+                label="Starting Date!"
+              />
+            </MuiPickersUtilsProvider>
+          </Collapse>
+          {/* LOADING ANIMATION BELOW */}
+          <Collapse>
+          
+          
+          
+          </Collapse>
+        </>
+      )}
+
+
+
+
+
+
+    </Formik>
   )
 
 }
