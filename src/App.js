4a99243e1a57eb7c09d15d08b45e7a414373b875
james-seymour <jsjamesemail@gmail.com>
2021-09-10 22:17:36 +1000

    added basic theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
 import './App.css';
+import { ThemeProvider } from "@material-ui/core"
+import { createTheme } from '@material-ui/core/styles';
+import { orange, blue } from '@material-ui/core/colors';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[500]
+    },
+    secondary: {
+      main: blue[500]
+    }
+  },
+  typography: {
+    fontSize
+  }
+})
 
 function App() {
   return (
