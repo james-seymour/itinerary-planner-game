48148320599a69975362c155f08a0d321024d72c
sllngshot <slingshotutube@gmail.com>
2021-09-11 14:34:07 +1000

    Velocity for player in game implemented

--- src/components/game/Car.jsx
+++ src/components/game/Car.jsx
@@ -2,17 +2,28 @@ import { makeStyles } from '@material-ui/core'
 import React from 'react'
 
 const useStyles = makeStyles((theme) => ({
-  car: {
+  rectangle: {
     width: 100,
     height: 100,
     position: 'absolute',
     backgroundColor: theme.palette.primary.main
+  },
+
+  circle: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: '100%'
   }
 }))
 
 function Car() {
   const classes = useStyles()
-  return <div className={classes.car}></div>
+  return <>
+      <div className={classes.rectangle}></div>
+      <div className={classes.circle}></div>
+    </>
 }
 
 export default Car
