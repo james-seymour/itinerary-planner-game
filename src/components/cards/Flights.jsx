7b08dd3d405cc52336bcbb941cc6218bbbbea8ad
james-seymour <jsjamesemail@gmail.com>
2021-09-11 11:56:57 +1000

    Found Flights widget on skyscanner

--- src/components/cards/Flights.jsx
+++ src/components/cards/Flights.jsx
@@ -2,11 +2,30 @@ import React from 'react'
 import { Box, Card, CardHeader,  } from '@material-ui/core'
 
 const Flights = ({ data }) => {
-  const { destination, startDate, endDate, latitude, longitude } = data
+  const { destination, startDateString, endDateString } = data
   return (
-    <div>
-      
-    </div>
+    <Box width={0.2} margin={4}>
+      <Card variant="outlined" >
+        <CardHeader
+          title={`Flights to ${destination}`}
+          subheader="Book a flight!"
+        />
+        {/* <iframe src={skyscannerWidgetSource} className={classes.skyScannerWidget}></iframe> */}
+        <div
+          data-skyscanner-widget="SearchWidget"
+          data-locale="en-US"
+          data-market="AU"
+          data-currency="AUD"
+          data-origin-name="'Brisbane'"
+          data-destination-name={`'${destination}'`}
+          data-flight-outbound-date={`${startDateString}`}
+          data-flight-inbound-date={`${endDateString}`}
+          data-widget-border-radius="10"
+          data-powered-by-size="0"
+          data-widget-padding="1rem"
+        ></div>
+      </Card>
+    </Box>
   )
 }
 
---
+++ src/components/cards/HotelsWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HotelsWidget = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HotelsWidget
